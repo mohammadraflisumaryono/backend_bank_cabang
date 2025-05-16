27c4d3700c5839bb7bdf051de310de58dc2dabd9
mohammadraflisumaryono <mohammadraflisumaryono@gmail.com>
2025-05-16 16:56:44 +0700

    feat: Refactor LoanController methods for loan operations and improve validation

--- src/controllers/loan-controller.js
+++ src/controllers/loan-controller.js
@@ -1,28 +1,10 @@
 const Joi = require("joi");
 const { ApplicationError, ValidationError } = require("../helpers/error-handler");
-const { encryptAes } = require("../helpers/security");
-const dayjs = require("dayjs");
-const LoanModel = require("../models/loan-model");
 const LoanModel = require("../models/loan-model");
 
 class LoanController {
     static addLoan = async (req, res, next) => {
         try {
-            // CREATE TABLE`loans`(
-            //     `id` int NOT NULL AUTO_INCREMENT,
-            //     `customer_id` int NOT NULL,
-            //     `branch_id` int NOT NULL,
-            //     `amount_plafond` decimal(15, 2) NOT NULL,
-            //     `interest_rate` float DEFAULT NULL,
-            //     `loan_date` date DEFAULT NULL,
-            //     `term_months` int DEFAULT NULL,
-            //     `status` enum('active', 'completed', 'defaulted') DEFAULT 'active',
-            //         PRIMARY KEY(`id`) USING BTREE,
-            //             KEY`customer_id`(`customer_id`),
-            //             KEY`branch_id`(`branch_id`),
-            //             CONSTRAINT`loans_ibfk_1` FOREIGN KEY(`customer_id`) REFERENCES`customers`(`id`),
-            //                 CONSTRAINT`loans_ibfk_2` FOREIGN KEY(`branch_id`) REFERENCES`kantor_units`(`id`)
-            //   ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE =utf8mb4_0900_ai_ci;
             const schema = Joi.object({
                 customer_id: Joi.number().required(),
                 branch_id: Joi.number().required(),
@@ -31,124 +13,120 @@ class LoanController {
                 loan_date: Joi.date().optional(),
                 term_months: Joi.number().optional(),
                 status: Joi.string().valid("active", "completed", "defaulted").optional(),
-
             });
 
             const { error } = schema.validate(req.body);
             if (error) return next(new ValidationError(error.message));
-
-            const userExist = await LoanModel.findByUsernameOrEmail(req.body.username);
-            if (userExist) {
-                return res.status(400).json({
-                    code: 101,
-                    message: `Username or email already exists.`,
-                    data: null,
-                });
-            }
-
 
             const data = await LoanModel.insertLoan(req.body);
 
             return res.status(201).json({
                 code: 201,
-                message: "Loans created successfully",
-                data: data,
+                message: "Loan created successfully",
+                data,
             });
         } catch (error) {
             return next(new ApplicationError(error.message));
         }
     };
 
-    static getAllUsers = async (req, res, next) => {
+    static getAllLoans = async (req, res, next) => {
         try {
-            const users = await LoanModel.findAll();
+            const loans = await LoanModel.getAllLoans();
             return res.status(200).json({
                 code: 0,
-                message: "Users retrieved successfully",
-                data: users,
+                message: "Loans retrieved successfully",
+                data: loans,
             });
         } catch (error) {
             return next(new ApplicationError(error.message));
         }
     };
 
-    static getUserById = async (req, res, next) => {
+    static getLoanById = async (req, res, next) => {
         try {
             const schema = Joi.object({ id: Joi.number().required() });
             const { error } = schema.validate(req.params);
             if (error) return next(new ValidationError(error.message));
 
-            const user = await LoanModel.findOneByID(req.params.id);
-            if (!user) {
+            const loan = await LoanModel.getLoanById(req.params.id);
+            if (!loan) {
                 return res.status(404).json({
                     code: 102,
-                    message: "User not found",
+                    message: "Loan not found",
                     data: null,
                 });
             }
 
             return res.status(200).json({
                 code: 0,
-                message: "User retrieved successfully",
-                data: user,
+                message: "Loan retrieved successfully",
+                data: loan,
             });
         } catch (error) {
             return next(new ApplicationError(error.message));
         }
     };
 
-    static updateUser = async (req, res, next) => {
+    static updateLoan = async (req, res, next) => {
         try {
-            const schema = Joi.object({
-                username: Joi.string().optional(),
-                email: Joi.string().email().optional(),
-                password: Joi.string().min(5).optional(),
+            const schemaParams = Joi.object({ id: Joi.number().required() });
+            const schemaBody = Joi.object({
+                customer_id: Joi.number().optional(),
+                branch_id: Joi.number().optional(),
+                amount_plafond: Joi.number().optional(),
+                interest_rate: Joi.number().optional(),
+                loan_date: Joi.date().optional(),
+                term_months: Joi.number().optional(),
+                status: Joi.string().valid("active", "completed", "defaulted").optional(),
             });
-            const { error } = schema.validate(req.body);
-            if (error) return next(new ValidationError(error.message));
 
-            const user = await LoanModel.findOneByID(req.params.id);
-            if (!user) {
+            const { error: errorParams } = schemaParams.validate(req.params);
+            if (errorParams) return next(new ValidationError(errorParams.message));
+
+            const { error: errorBody } = schemaBody.validate(req.body);
+            if (errorBody) return next(new ValidationError(errorBody.message));
+
+            const existingLoan = await LoanModel.getLoanById(req.params.id);
+            if (!existingLoan) {
                 return res.status(404).json({
                     code: 103,
-                    message: "User not found",
+                    message: "Loan not found",
                     data: null,
                 });
             }
 
-            if (req.body.password) req.body.password = await encryptAes(req.body.password);
-            const updatedUser = await LoanModel.update({ id: req.params.id }, req.body);
-
+            const updatedLoan = await LoanModel.updateLoan(req.params.id, req.body);
             return res.status(200).json({
                 code: 0,
-                message: "User updated successfully",
-                data: updatedUser,
+                message: "Loan updated successfully",
+                data: updatedLoan,
             });
         } catch (error) {
             return next(new ApplicationError(error.message));
         }
     };
 
-    static deleteUser = async (req, res, next) => {
+    static deleteLoan = async (req, res, next) => {
         try {
             const schema = Joi.object({ id: Joi.number().required() });
             const { error } = schema.validate(req.params);
             if (error) return next(new ValidationError(error.message));
 
-            const user = await LoanModel.findOneByID(req.params.id);
-            if (!user) {
+            const loan = await LoanModel.getLoanById(req.params.id);
+            if (!loan) {
                 return res.status(404).json({
                     code: 104,
-                    message: "User not found",
+                    message: "Loan not found",
                     data: null,
                 });
             }
 
-            await LoanModel.delete({ id: req.params.id });
+            const result = await LoanModel.deleteLoan(req.params.id);
             return res.status(200).json({
                 code: 200,
-                message: "User deleted successfully",
-                data: null,
+                message: result.message,
+                data: result.deletedData,
             });
         } catch (error) {
             return next(new ApplicationError(error.message));
